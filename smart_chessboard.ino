e4acdbd178af4e850aaef3a97034f4231e0be1f4
Prometheus <cg.riccardo@gmail.com>
2020-09-26 20:14:58 +0200

    minor bug fixed

--- smart_chessboard.ino
+++ smart_chessboard.ino
@@ -7,8 +7,8 @@
 #define BISHOP 0x20
 #define QUEEN 0x40
 #define KING 0x80
-byte[] ch_bitmap = {0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff};
-byte[][] ch_ext = {
+byte ch_bitmap[] = {0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff};
+byte ch_ext[][8] = {
     {ROOK|BLACK, KNIGHT|BLACK, BISHOP|BLACK, QUEEN|BLACK, KING|BLACK, BISHOP|BLACK, KNIGHT|BLACK, ROOK|BLACK},
     {PAWN|BLACK, PAWN|BLACK, PAWN|BLACK, PAWN|BLACK, PAWN|BLACK, PAWN|BLACK, PAWN|BLACK, PAWN|BLACK},
     {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
@@ -21,10 +21,10 @@ byte[][] ch_ext = {
 #define PIECE_IS(type, row, col) (ch_ext[row][col]&type)                        //equal zero if type doesn't matches, greater than zero otherwise
 #define PIECE_IS_ALL(type, row, col) ((ch_ext[row][col]&type) == type)
 #define OPPONENT(player) player^0x03
-
+//TODO SET_LED
 //player can be 1(white) or 2(black) instead of 0 and 1 for simpler controls
 byte player = 0x01, player1, player2;											//player is the active player, player1 is the player on the bottom, player2 is the player on the top
-bool[] castling = {true, true};
+bool castling[] = {true, true};
 #define CAN_CASTLE(pl) (castling[pl-1])
 
 void ch_monitoring(){
@@ -48,17 +48,17 @@ void ch_monitoring(){
     }
 }
 
-void find_moves(row, col){
+void find_moves(int row, int col){
     int direction = -1;
     if(!PIECE_IS(player, row, col)){											//a player can only move his pieces
         error_handler();
         return;
     }
     switch (ch_ext[row][col]) {
-        case
-        case PAWN|player2:                                                      //player2 is the player on top (color doesn't matter)
-            direction = 1;														//player2's pawns can only eat and move downward, player1's pawns can only eat and move upward
-        case PAWN|player1:
+        case PAWN|BLACK:
+        case PAWN|WHITE:
+			//player2 is the player on top (color doesn't matter)
+			if(PIECE_IS(player2, row, col)) direction = 1;						//player2's pawns can only eat and move downward, player1's pawns can only eat and move upward
 			//(row == direction || row == 7+direction) -> true only if the pawn is in the original position
 			//if pawn belongs to the top player its original position is row 1 and direction is 1-> row == direction (a pawn of the bottom player can never be in row -1)
 			//if pawn belongs to the bottom player its original position is  row 6 and direction is -1 -> row == 7+direction (a pawn of the top player can never be in row 8)
@@ -144,7 +144,7 @@ void find_moves(row, col){
 				SET_LED(BLUE, row, col + 2);
 			//long castling
 			if(CAN_CASTLE(player) && !PIECE_IS(PRESENT, row, col - 1) && !PIECE_IS(PRESENT, row, col - 2))
-				SET_LED(BLUE, row, col - 2)
+				SET_LED(BLUE, row, col - 2);
             break;
         case QUEEN|BLACK:
         case QUEEN|WHITE:
@@ -198,7 +198,7 @@ void find_moves(row, col){
     }
 }
 
-bool suicidal_move(old_row, old_col, new_row, new_col){
+bool suicidal_move(int old_row, int old_col, int new_row, int new_col){
 	//TODO disable interrupt
 	byte backup = ch_ext[new_row][new_col];
 	ch_ext[new_row][new_col] = ch_ext[old_row][old_col];						//moving the piece into his new position
@@ -212,17 +212,18 @@ bool suicidal_move(old_row, old_col, new_row, new_col){
 
 	return result;
 }
-bool suicidal_castling(k_row, old_k_col, new_k_col){
+
+bool suicidal_castling(int k_row, int old_k_col, int new_k_col){
 	//TODO disable interrupt
 	//no need to backup because new positions are free
 	ch_ext[k_row][new_k_col] = ch_ext[k_row][old_k_col];						//moving the king into his new position
 	ch_ext[k_row][old_k_col] = 0x00;
 	if(old_k_col < new_k_col){													//short castling -> rook is on the left
-		ch_ext[k_row][k_col - 1] = ch_ext[k_row][7];
+		ch_ext[k_row][new_k_col - 1] = ch_ext[k_row][7];
 		ch_ext[k_row][7] = 0x00;
 	}
 	else{
-		ch_ext[k_row][k_col + 1] = ch_ext[k_row][0];							//long castling -> rook is on the right
+		ch_ext[k_row][new_k_col + 1] = ch_ext[k_row][0];							//long castling -> rook is on the right
 		ch_ext[k_row][0] = 0x00;
 	}
 
@@ -231,20 +232,20 @@ bool suicidal_castling(k_row, old_k_col, new_k_col){
 	ch_ext[k_row][old_k_col] = ch_ext[k_row][new_k_col];						//restoring old positions
 	ch_ext[k_row][new_k_col] = 0x00;
 	if(old_k_col < new_k_col){
-		ch_ext[k_row][7] = ch_ext[k_row][k_col - 1];
-		ch_ext[k_row][k_col - 1] = 0x00;
+		ch_ext[k_row][7] = ch_ext[k_row][new_k_col - 1];
+		ch_ext[k_row][new_k_col - 1] = 0x00;
 	}
 	else{
-		ch_ext[k_row][0] = ch_ext[k_row][k_col + 1];
-		ch_ext[k_row][k_col + 1] = 0x00;
+		ch_ext[k_row][0] = ch_ext[k_row][new_k_col + 1];
+		ch_ext[k_row][new_k_col + 1] = 0x00;
 	}
 	//TODO enable interrupt
 }
 
-bool check(player){
+bool check(byte player){
 	//find king
 	int k_row, k_col;
-	for(int row = 0; col < 8; col++){
+	for(int row = 0; row < 8; row++){
 		for(int col = 0; col < 8; col++){
 			if(PIECE_IS_ALL(KING|player, row, col)){
 				k_row = row;
@@ -256,7 +257,7 @@ bool check(player){
 	for(int i = 0; i < 8 - k_row; i++){
 		if(PIECE_IS(PRESENT, k_row + i, k_col)){
 			if(i == 1 && PIECE_IS_ALL(KING|OPPONENT(player), k_row + i, k_col)) return true;
-			if(PIECE_IS(QUEEN|ROOK, k_row + i, k_col) && PIECE_IS(OPPONENT(player))) return true;
+			if(PIECE_IS(QUEEN|ROOK, k_row + i, k_col) && PIECE_IS(OPPONENT(player), k_row + i, k_col)) return true;
 			break;
 		}
 	}
@@ -264,7 +265,7 @@ bool check(player){
 	for(int i = 0; i < k_row; i++){
 		if(PIECE_IS(PRESENT, k_row - i, k_col)){
 			if(i == 1 && PIECE_IS_ALL(KING|OPPONENT(player), k_row - i, k_col)) return true;
-			if(PIECE_IS(QUEEN|ROOK, k_row - i, k_col) && PIECE_IS(OPPONENT(player))) return true;
+			if(PIECE_IS(QUEEN|ROOK, k_row - i, k_col) && PIECE_IS(OPPONENT(player), k_row - i, k_col)) return true;
 			break;
 		}
 	}
@@ -272,7 +273,7 @@ bool check(player){
 	for(int i = 0; i < k_col; i++){
 		if(PIECE_IS(PRESENT, k_row, k_col - i)){
 			if(i == 1 && PIECE_IS_ALL(KING|OPPONENT(player), k_row, k_col - i)) return true;
-			if(PIECE_IS(QUEEN|ROOK, k_row, k_col - i) && PIECE_IS(OPPONENT(player))) return true;
+			if(PIECE_IS(QUEEN|ROOK, k_row, k_col - i) && PIECE_IS(OPPONENT(player), k_row, k_col - i)) return true;
 			break;
 		}
 	}
@@ -280,7 +281,7 @@ bool check(player){
 	for(int i = 0; i < 8 - k_col; i++){
 		if(PIECE_IS(PRESENT, k_row, k_col + i)){
 			if(i == 1 && PIECE_IS_ALL(KING|OPPONENT(player), k_row, k_col + i)) return true;
-			if(PIECE_IS(QUEEN|ROOK, k_row, k_col + i) && PIECE_IS(OPPONENT(player))) return true;
+			if(PIECE_IS(QUEEN|ROOK, k_row, k_col + i) && PIECE_IS(OPPONENT(player), k_row, k_col + i)) return true;
 			break;
 		}
 	}
@@ -288,7 +289,7 @@ bool check(player){
 	for(int i = 0; i < 8 - k_row &&  i < k_col; i++){
 		if(PIECE_IS(PRESENT, k_row + i, k_col - i)){
 			if(i == 1 && PIECE_IS_ALL(KING|OPPONENT(player), k_row + i, k_col - i)) return true;
-			if(PIECE_IS(QUEEN|BISHOP, k_row + i, k_col - i) && PIECE_IS(OPPONENT(player))) return true;
+			if(PIECE_IS(QUEEN|BISHOP, k_row + i, k_col - i) && PIECE_IS(OPPONENT(player), k_row + i, k_col - i)) return true;
 			break;
 		}
 	}
@@ -296,15 +297,15 @@ bool check(player){
 	for(int i = 0; i < 8 - k_row &&  i < 8 - k_col; i++){
 	   if(PIECE_IS(PRESENT, k_row + i, k_col + i)){
 		   if(i == 1 && PIECE_IS_ALL(KING|OPPONENT(player), k_row + i, k_col + i)) return true;
-		   if(PIECE_IS(QUEEN|BISHOP, k_row + i, k_col + i) && PIECE_IS(OPPONENT(player))) return true;
+		   if(PIECE_IS(QUEEN|BISHOP, k_row + i, k_col + i) && PIECE_IS(OPPONENT(player), k_row + i, k_col + i)) return true;
 		   break;
 	   }
-   }
+	}
 	//down-left
 	for(int i = 0; i < k_row &&  i < k_col; i++){
 		if(PIECE_IS(PRESENT, k_row - i, k_col - i)){
 			if(i == 1 && PIECE_IS_ALL(KING|OPPONENT(player), k_row - i, k_col - i)) return true;
-			if(PIECE_IS(QUEEN|BISHOP, k_row - i, k_col - i) && PIECE_IS(OPPONENT(player))) return true;
+			if(PIECE_IS(QUEEN|BISHOP, k_row - i, k_col - i) && PIECE_IS(OPPONENT(player), k_row - i, k_col - i)) return true;
 			break;
 		}
 	}
@@ -312,14 +313,14 @@ bool check(player){
 	for(int i = 0; i < k_row &&  i < 8 - k_col; i++){
 		if(PIECE_IS(PRESENT, k_row - i, k_col + i)){
 			if(i == 1 && PIECE_IS_ALL(KING|OPPONENT(player), k_row - i, k_col + i)) return true;
-			if(PIECE_IS(QUEEN|BISHOP, k_row - i, k_col + i) && PIECE_IS(OPPONENT(player))) return true;
+			if(PIECE_IS(QUEEN|BISHOP, k_row - i, k_col + i) && PIECE_IS(OPPONENT(player), k_row - i, k_col + i)) return true;
 			break;
 		}
 	}
 	//pawn
 	if(player == player2)														//if player is the player on top the opponent's pawn can eat only if they are below the king
 		if(PIECE_IS_ALL(PAWN|OPPONENT(player), k_row - 1, k_col - 1) || PIECE_IS_ALL(PAWN|OPPONENT(player), k_row - 1, k_col + 1)) return true;
-	else{
+	else
 		if(PIECE_IS_ALL(PAWN|OPPONENT(player), k_row + 1, k_col - 1) || PIECE_IS_ALL(PAWN|OPPONENT(player), k_row + 1, k_col + 1)) return true;
 	//knight
 	if(k_row < 7 && k_col < 5 && PIECE_IS_ALL(KNIGHT|OPPONENT(player), k_row + 1, k_col + 2)) return true;
@@ -332,3 +333,9 @@ bool check(player){
 	if(k_row > 2 && k_col < 7 && PIECE_IS_ALL(KNIGHT|OPPONENT(player), k_row - 2, k_col + 1)) return true;
 	if(k_row > 2 && k_col > 1 && PIECE_IS_ALL(KNIGHT|OPPONENT(player), k_row - 2, k_col - 1)) return true;
 }
+
+byte ch_read(){}//TODO
+
+void error_handler(){}//TODO
+
+void show_moves(){}//TODO
