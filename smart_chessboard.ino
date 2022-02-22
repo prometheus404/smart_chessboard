e3177019ee1f58b0321a7aed4d6f469ce9bb82c8
Riccardo Conforto Galli <cg.riccardo@tuta.io>
2022-02-22 10:47:46 +0100

    Refactoring

--- smart_chessboard.ino
+++ smart_chessboard.ino
@@ -1,39 +1,4 @@
-#include<FastLED.h>
-//#include<TaskScheduler.h>
-#include <LiquidCrystal.h>
-#include "struct.h"
-#include <stdlib.h>
-
-#define NUM_LEDS 	64
-#define TOP 		2
-#define BOTTOM 		3
-#define LED_PIN 	4
-#define SHIFT_MODE 	5
-#define SHIFT_CLK 	6
-#define SHIFT_DATA 	7
-#define LCD_RS 		8
-#define LCD_EN 		9
-#define LCD_D4 		10
-#define LCD_D5 		11
-#define LCD_D6 		12
-#define LCD_D7 		13
-
-LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-CRGB leds[NUM_LEDS];
-Game game;
-
-#define LED_GREEN 	CRGB::Green
-#define LED_RED 	CRGB::Red
-#define LED_YELLOW 	CRGB::Yellow
-#define LED_PURPLE 	CRGB::Purple
-#define LED_BLUE 	CRGB::Blue
-#define LED_WHITE 	CRGB::White
-#define LED_BLACK 	CRGB::Black
-
-
-#define SET_LED(color, row, col) 	(leds[(row*8) + col] = color)
-#define LED_IS(color, row, col) 	(leds[(row*8) + col] == color)
-
+#include"common.h"
 //including rules
 #include"tris.h"
 #include"test.h"
@@ -126,7 +91,7 @@ void update_lcd(){
 
 //check if a piece is placed on the board, if found set up the game
 void main_loop(){
-	for(int i = 0; i < 8; i++)
+	for(int i = 0; i < 8; i++)  
 		if(game.actual[i])
 			setup_game[selected]();
 }
